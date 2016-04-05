7cd4a959188cc82a999c762e2f200c24c40672ad
najaestevenson <najaestevenson@gmail.com>
2016-04-05 07:40:40 -0400

    adding another bubble sort test.

--- test/algorithms/sortAlgorithms.java
+++ test/algorithms/sortAlgorithms.java
@@ -20,13 +20,16 @@ public void setUp(){
 	public void bubbleSorttest() {
 		//Using J-unit I can do test driven development. 
 		//I write the code i want to test. I know my code will error out at first.
-		BubbleSort bs = new BubbleSort();// no class called BubbleSort in package
-		int [] array = new int[]{2,55,23,11,9,43,33,17};
+		 array = new int[]{2,55,23,11,9,43,33,17};
 		bs.unsortedArray(array);
 		int [] actual = bs.bubbleSort(array);
 		int [] expected = {2,9,11,17,23,33,43,55};
 		bs.printSortedArray(array);
 		assertArrayEquals(expected, actual);
+	}
+	@Test
+	public void anotherBubbleSortTest(){
+		
 	}
 
 }
