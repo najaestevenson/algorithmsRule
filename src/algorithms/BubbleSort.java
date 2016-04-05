305d3fef0f09585edbb35b04b1ce52d05d5ea9fa
najaestevenson <najaestevenson@gmail.com>
2016-04-05 00:00:13 -0400

    J-unit test to do test driven design

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+class BubbleSort {
+
+	public void unsortedArray(int [] a){
+		for(int i = 0; i< a.length; i++){
+			System.out.println("Array before bubble sort"+ a[i]+"");
+		}
+	}
+	public int [] bubbleSort(int[] array){
+		int n = array.length;
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for(int j =1;j<(n-i);j++){
+				if(array[j-1]> array[j]){
+					temp = array[j-1];
+					array[j-1]=array[j];
+					array[j]= temp;
+				}
+				
+			}
+			
+		} return array;
+	}
+	public void printSortedArray(int [] a){
+		for(int i = 0; i< a.length; i++){
+			System.out.println("Array after bubble sort"+ a[i]+"");	
+	}
+		
+}
+}
