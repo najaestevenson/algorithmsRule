a65ba40435bf3897d921596f3000861cc56098b6
najaestevenson <najaestevenson@gmail.com>
2016-04-06 18:46:40 -0400

    added 2 methods to print sorted and unsorted arrays. Added class members

---
+++ test/algorithms/QuickSort.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+class QuickSort {
+	
+private int arr[];
+private int length[];
+	public void printUnsortedArray(int[] array) {
+		for(int i = 0; i< array.length; i++){
+			System.out.println("Array before bubble sort"+ array[i]+"");
+		}
+		
+	}
+
+	public int[] quickSort(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void printsortedArray(int[] array) {
+		for(int i = 0; i< array.length; i++){
+			System.out.println("Array after bubble sort"+ array[i]+"");	
+	}
+		
+	}
+
+}
