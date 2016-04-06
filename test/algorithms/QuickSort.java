8c530ea9879f688062afcd6495353ce26a59c257
najaestevenson <najaestevenson@gmail.com>
2016-04-06 19:06:56 -0400

    updating sort and quick sort in quicksort class. updated quicksort test

--- test/algorithms/QuickSort.java
+++ test/algorithms/QuickSort.java
@@ -3,15 +3,24 @@ package algorithms;
 class QuickSort {
 	
 private int arr[];
-private int length[];
+private int length;
 	public void printUnsortedArray(int[] array) {
 		for(int i = 0; i< array.length; i++){
 			System.out.println("Array before bubble sort"+ array[i]+"");
 		}
 		
 	}
+	public int[] sort( int[] inputArr){
+		if(inputArr== null|| inputArr.length ==0){
+			return null;
+		}
+		this.arr = inputArr;
+		length = inputArr.length;
+		return quickSort(0, length-1);
+		}
 
-	public int[] quickSort(int[] array) {
+	
+	public int[] quickSort(int lowerIndex, int higherIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- test/algorithms/sortAlgorithms.java
+++ test/algorithms/sortAlgorithms.java
@@ -9,11 +9,13 @@ import algorithms.BubbleSort;
 
 public class sortAlgorithms {
 BubbleSort bs;
+QuickSort qs;
 int [] array;
 
 @Before
 public void setUp(){
 	bs = new BubbleSort();
+	qs = new QuickSort();
 	
 }
 	@Test
@@ -21,10 +23,10 @@ public void setUp(){
 		//Using J-unit I can do test driven development. 
 		//I write the code i want to test. I know my code will error out at first.
 		 array = new int[]{2,55,23,11,9,43,33,17};
-		bs.unsortedArray(array);
-		int [] actual = bs.bubbleSort(array);
+		bs.unsortedArray(array);//print unsorted array
+		int [] actual = bs.bubbleSort(array);//call sort method
 		int [] expected = {2,9,11,17,23,33,43,55};
-		bs.printSortedArray(array);
+		bs.printSortedArray(array);// print sorted array
 		assertArrayEquals(expected, actual);
 	}
 	@Test
@@ -33,9 +35,18 @@ public void setUp(){
 	bs.unsortedArray(array);//printing unsorted array
 	int[] actual= bs.bubbleSort(array);// calling bubble sort on array 
 	int[] expect = {1,2,3,5,6,7,8,9};
-	bs.printSortedArray(array);
+	bs.printSortedArray(array);//print sorted array
 	assertArrayEquals(expect, actual);
 	}
 
+	@Test
+	public void quickSortTest(){
+		array = new int[]{29,45,77,84,1,93,25,82};
+		qs.printUnsortedArray(array);
+		int [] actual = qs.sort(array);
+		int [] expected = {1,25,29,45,77,82,84,93};
+		qs.printsortedArray(array);
+		assertArrayEquals(expected, actual);
+	}
 }
 
