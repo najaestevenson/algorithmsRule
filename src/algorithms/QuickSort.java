0b0a07fc0f6e1772dec1ba750fe90910fcc865aa
najaestevenson <najaestevenson@gmail.com>
2016-04-13 21:43:50 -0400

    Looking over my code and realizing that I have some redundancy in my code. Not good.

---
+++ src/algorithms/QuickSort.java
@@ -0,0 +1,68 @@
+package algorithms;
+
+class QuickSort {
+
+	private int arr[];
+	private int length;
+
+	public void printUnsortedArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Array before Quick Sort" + array[i] + "");
+		}
+
+	}
+
+	public void sort(int[] inputArr) {
+		if (inputArr == null || inputArr.length == 0) {
+			return;
+		}
+		this.arr = inputArr;
+		length = inputArr.length;
+		quickSort(0, length - 1);
+		
+	}
+
+	public void quickSort(int lowerIndex, int higherIndex) {
+		int i = lowerIndex;
+		int j = higherIndex;
+		int pivot = arr[lowerIndex + (higherIndex - lowerIndex) / 2];
+		while (i <= j) {
+			while (arr[i] < pivot) {
+				i++;
+			}
+			while (arr[j] > pivot) {
+				j--;
+			}
+			if (i <= j) {
+				exchangeNumbers(i, j);
+				i++;
+				j--;
+			}
+		}
+		if (lowerIndex < j) {
+			quickSort(lowerIndex, j);
+		}
+		if (i < higherIndex) {
+			quickSort(i, higherIndex);
+		}
+
+	}
+
+	public void exchangeNumbers(int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public int[] printsortedArray(int[] array) {
+		this.sort(array);
+		for (int i : array) {
+			System.out.print(i);
+			System.out.print(",");
+			
+		}return array;
+		}
+}
+	
+
+
